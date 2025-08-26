bc5444d793f866c9fa1a334bbe3057304355ae87
Kirtan Gajjar <w.kgajjar@gmail.com>
2025-08-26 16:58:16 +0530

    added base path

--- vite.config.js
+++ vite.config.js
@@ -10,7 +10,7 @@ export default defineConfig({
   ],
   define:
   {
-    "VITE_API": process.env.VITE_API 
+    "VITE_API": JSON.stringify(process.env.VITE_API) 
   },
   base: process.env.VITE_BASE_PATH || "/XChangeX"
 })
